4ebe4a7f9dccfad4d6b575412bb5facc6f507739
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-23 22:32:55 -0500

    Read data from Images database

--- wheres-waldo-app/src/App.js
+++ wheres-waldo-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,8 +22,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
+const querySnapshot = await getDocs(collection(db, 'Images'));
 
 function App() {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
     return (
         <div className='App'>
             <header className='App-header'></header>
