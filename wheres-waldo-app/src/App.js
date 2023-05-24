a23b8ac1636f77bbdf82edffbc039c2d1e483930
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-23 22:27:05 -0500

    Initialize Cloud Firestore

--- wheres-waldo-app/src/App.js
+++ wheres-waldo-app/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,6 +19,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
 
 function App() {
     return (
