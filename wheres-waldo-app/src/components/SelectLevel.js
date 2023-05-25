152ad878b9eb582769e3b1eeaabb1c8704167407
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-24 22:07:35 -0500

    Add aditional divs

--- wheres-waldo-app/src/components/SelectLevel.js
+++ wheres-waldo-app/src/components/SelectLevel.js
@@ -1,10 +1,12 @@
-import { Children } from 'react';
+import React from 'react';
 
-const SelectLevel = () => {
+const SelectLevel = (children) => {
     return (
         <div className='level-select'>
-            <div className='level-select-title'>Select Level</div>
-            {Children}
+            <div className='level-select-title'>
+                <h2>Select Level</h2>
+            </div>
+            {/* {children} */}
         </div>
     );
 };
