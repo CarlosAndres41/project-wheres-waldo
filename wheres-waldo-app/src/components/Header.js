515915dd2ed91b465e7f29c04eed1ad01661e006
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-24 20:59:49 -0500

    Add basic HTML structure to Header

--- wheres-waldo-app/src/components/Header.js
+++ wheres-waldo-app/src/components/Header.js
@@ -1,7 +1,12 @@
 const Header = () => {
     return (
-        <header>
-            <h1>Header</h1>
+        <header className='app-header'>
+            <div className='app-name-logo'>
+                <h1>Wimmelbilder App</h1>
+            </div>
+            <div className='header-buttons'>
+                <button className='btn btn-primary'>Leaaderboard</button>
+            </div>
         </header>
     );
 };
