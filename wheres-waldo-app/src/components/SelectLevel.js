3c0ba56b0ab19121b65201efa5c8b5ae3dc0f659
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-24 21:35:55 -0500

    Add Select Level div

--- wheres-waldo-app/src/components/SelectLevel.js
+++ wheres-waldo-app/src/components/SelectLevel.js
@@ -1,7 +1,12 @@
 import { Children } from 'react';
 
 const SelectLevel = () => {
-    return <div className='level-select'>{Children}</div>;
+    return (
+        <div className='level-select'>
+            <div className='level-select-title'>Select Level</div>
+            {Children}
+        </div>
+    );
 };
 
 export default SelectLevel;
