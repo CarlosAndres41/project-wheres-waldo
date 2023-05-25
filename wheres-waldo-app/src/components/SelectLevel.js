a2bc56b051eb2bf58704279b893828c14b959b6a
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-24 22:13:59 -0500

    Refactor component to deal with children components

--- wheres-waldo-app/src/components/SelectLevel.js
+++ wheres-waldo-app/src/components/SelectLevel.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const SelectLevel = (children) => {
+const SelectLevel = ({ children }) => {
     return (
         <div className='level-select'>
             <div className='level-select-title'>
                 <h2>Select Level</h2>
             </div>
-            {/* {children} */}
+            {children}
         </div>
     );
 };
