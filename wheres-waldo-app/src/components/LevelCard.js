505f156dcaadf4ed841d54a5c036616de94801f5
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-24 23:40:25 -0500

    Add LevelCard HTML structure including props

--- wheres-waldo-app/src/components/LevelCard.js
+++ wheres-waldo-app/src/components/LevelCard.js
@@ -2,7 +2,26 @@ const LevelCard = (props) => {
     const { name, author, source, image, index } = props;
     return (
         <div className='level-card'>
-            <h1>Level Card</h1>
+            <div className='card-header'>
+                <h3>Level {index + 1}</h3>
+            </div>
+            <div className='card-name'>
+                <h4>{name}</h4>
+            </div>
+            <div className='card-img'>
+                <img src={image} alt={name}></img>
+            </div>
+            <div className='card-chars'></div>
+            <div className='card-footer'>
+                <p>
+                    <span>Author:</span> {author}
+                </p>
+                <p>
+                    <a href={source} target='_blank' rel='noreferrer'>
+                        Source
+                    </a>
+                </p>
+            </div>
         </div>
     );
 };
