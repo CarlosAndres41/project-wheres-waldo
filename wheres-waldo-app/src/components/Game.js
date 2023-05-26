d7f88a98498b8e22e513902fc67cd2584e4f6d49
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-26 18:56:48 -0500

    Create initial Game component

---
+++ wheres-waldo-app/src/components/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Game = () => {
+    return (
+        <div className='main-game'>
+            <h1>Game</h1>
+            <button>Return to Select Level</button>
+        </div>
+    );
+};
+
+export default Game;
