bf4a3ae14da26bddce244d273ec5580538642be5
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-26 19:16:45 -0500

    Display level accordingly to the level selected

--- wheres-waldo-app/src/components/Game.js
+++ wheres-waldo-app/src/components/Game.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Game = (props) => {
     const returnToSelect = props.returnToSelect;
+    const level = props.level;
 
     return (
         <div className='main-game'>
-            <h1>Game</h1>
+            <h1>Level {level} </h1>
             <button onClick={returnToSelect}>Return to Select Level</button>
         </div>
     );
