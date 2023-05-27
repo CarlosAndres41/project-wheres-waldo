6713d6457c033545b39d67dbde662b4f08753881
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-26 19:56:33 -0500

    Create basic DropdownContent component

---
+++ wheres-waldo-app/src/components/Dropdown-content.js
@@ -0,0 +1,11 @@
+const DropdownContent = () => {
+    return (
+        <div className='dropdown-content'>
+            <p>Character 1</p>
+            <p>Character 2</p>
+            <p>Character 3</p>
+        </div>
+    );
+};
+
+export default DropdownContent;
