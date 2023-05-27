3dd150d27cd7ff77b603c4845092fa59d1ee48e3
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-26 19:42:18 -0500

    Display Level name and image

--- wheres-waldo-app/src/components/Game.js
+++ wheres-waldo-app/src/components/Game.js
@@ -3,10 +3,18 @@ import React from 'react';
 const Game = (props) => {
     const returnToSelect = props.returnToSelect;
     const level = props.level;
+    const data = props.data;
 
     return (
         <div className='main-game'>
-            <h1>Level {level} </h1>
+            <div className='game-header'>
+                <h1>
+                    Level {level} - {data[level - 1].name}
+                </h1>
+            </div>
+            <div className='game-board'>
+                <img src={data[level - 1].image} alt={data[level - 1].name} />
+            </div>
             <button onClick={returnToSelect}>Return to Select Level</button>
         </div>
     );
