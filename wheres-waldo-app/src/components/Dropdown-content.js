c0e868989dbda76d45a014dff7eea4b2e3e799a5
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-26 20:23:18 -0500

    Add style to dropdown from props

--- wheres-waldo-app/src/components/Dropdown-content.js
+++ wheres-waldo-app/src/components/Dropdown-content.js
@@ -1,6 +1,6 @@
-const DropdownContent = () => {
+const DropdownContent = (props) => {
     return (
-        <div className='dropdown-content'>
+        <div className='dropdown-content' style={props.style}>
             <p>Character 1</p>
             <p>Character 2</p>
             <p>Character 3</p>
