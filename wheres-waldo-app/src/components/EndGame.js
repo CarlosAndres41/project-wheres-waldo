f3f00f99489298c5dfba145f73462b6d541e4cab
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-30 00:06:28 -0500

    Add handlePlayAgain function

--- wheres-waldo-app/src/components/EndGame.js
+++ wheres-waldo-app/src/components/EndGame.js
@@ -1,11 +1,18 @@
 const EndGame = (props) => {
+    const handlePlayAgain = () => {
+        props.setFound([false, false, false]);
+        props.setFoundAll(false);
+    };
+
     return (
         <div className={props.foundAll ? 'end-game show' : 'end-game'}>
             <div className='message'>
                 <h1>Congratulations, you have won the game</h1>
             </div>
             <div className='end-game-btns'>
-                <button className='btn btn-secondary'>Play Again</button>
+                <button className='btn btn-secondary' onClick={handlePlayAgain}>
+                    Play Again
+                </button>
                 <button
                     className='btn btn-secondary'
                     onClick={props.returnToSelect}
