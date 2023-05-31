a735fd208a121a59ee39b9da4e06e7b5e7defc96
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-31 00:41:25 -0500

    Add Return Button, minor refactor

--- wheres-waldo-app/src/components/Leaderboard.js
+++ wheres-waldo-app/src/components/Leaderboard.js
@@ -1,10 +1,18 @@
-const Leaderboard = ({ children }) => {
+const Leaderboard = (props) => {
     return (
         <div className='level-select'>
             <div className='level-select-title'>
                 <h2>Leaderboard</h2>
             </div>
-            <div className='all-levels'>{children}</div>
+            <div className='all-levels'>{props.children}</div>
+            <div className='game-btn-container'>
+                <button
+                    onClick={props.returnToSelect}
+                    className='btn btn-secondary'
+                >
+                    Return to Select Level
+                </button>
+            </div>
         </div>
     );
 };
