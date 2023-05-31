4986de29e755d00c9d22e26581ec472155d37a35
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-31 00:07:41 -0500

    Refactor Leaderboard Based on selectLevel component

--- wheres-waldo-app/src/components/Leaderboard.js
+++ wheres-waldo-app/src/components/Leaderboard.js
@@ -1,5 +1,12 @@
-const Leaderboard = () => {
-    return <div> Leaderboard </div>;
+const Leaderboard = ({ children }) => {
+    return (
+        <div className='level-select'>
+            <div className='level-select-title'>
+                <h2>Leaderboard</h2>
+            </div>
+            <div className='all-levels'>{children}</div>
+        </div>
+    );
 };
 
 export default Leaderboard;
