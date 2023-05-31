3a47c3ae3881b8e3d774fe53aa74ce213b306f4c
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-31 01:13:44 -0500

    Map scores

--- wheres-waldo-app/src/components/Scoreboard.js
+++ wheres-waldo-app/src/components/Scoreboard.js
@@ -1,10 +1,17 @@
-const Scoreboard = () => {
+const Scoreboard = (props) => {
+    const scores = props.scores;
     return (
         <div className='scoreboard'>
             <div className='scoreboard-header'>
                 <h4>Player name:</h4>
                 <h4>Time:</h4>
             </div>
+            {scores.map((score, index) => (
+                <div className='scoreboard-row' key={index}>
+                    <p>{score.name}</p>
+                    <p>{score.time}</p>
+                </div>
+            ))}
         </div>
     );
 };
