1de8d01bc6dd734420a0ba02149adbcea349d0cf
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-31 01:09:00 -0500

    Create Scoreboard component

---
+++ wheres-waldo-app/src/components/Scoreboard.js
@@ -0,0 +1,12 @@
+const Scoreboard = () => {
+    return (
+        <div className='scoreboard'>
+            <div className='scoreboard-header'>
+                <h4>Player name:</h4>
+                <h4>Time:</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Scoreboard;
